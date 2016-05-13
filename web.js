a708a630457facd1b2cac98bd5aed2b9ba2d26c4
焦中肖 <jiaox99@163.com>
2016-05-13 16:33:16 +0800

    auth

--- web.js
+++ web.js
@@ -22,7 +22,7 @@ app.get( '/omni',
 const https = require( 'https' );
 
 var options = {
-	host : "https://account.teambition.com/oauth2/authorize",
+	host : "https://account.teambition.com/oauth2/access_token",
 	method : "POST",
 	client_id:"132a6150-0ae2-11e6-bfaa-a3d82dc1ed48",
 	client_secret:"09b48944-ec96-4f07-ae16-e88352fdc0bd",
