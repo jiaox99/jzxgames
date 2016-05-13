35e9a9d09db1a870d4fe40ced98cbe9cf67f5b72
焦中肖 <jiaox99@163.com>
2016-05-13 15:27:10 +0800

    initApp

--- web.js
+++ web.js
@@ -9,6 +9,22 @@ app.get( "/td", td.list );
 
 app.use(logfmt.requestLogger());
 app.use(express.static(__dirname + '/public'));
+app.set( "views", "./views" );
+app.set( "view engine", 'jade' );
+
+app.get( '/omni', 
+	function( req, res )
+	{
+		res.render( "omni", { title:"Hello, OmniPlan" } );
+	}
+ );
+
+app.get( '/omniLogin', 
+	function( req, res )
+	{
+		res.send( req.code );
+	}
+);
 
 app.get('/', function(req, res) {
   res.send('Hello World, 这是中国！');
